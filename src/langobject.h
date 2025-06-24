3c73e66814a466ca93bd5a619d0c4858f8a26cce
MurilloMMartins <murillomartins19@gmail.com>
2025-06-23 23:49:27 -0300

    Started developing the language objects

---
+++ src/langobject.h
@@ -0,0 +1,14 @@
+#pragma once
+
+typedef enum LangObjectKind {
+  INTEGER
+} lang_object_kind_t;
+
+typedef union LangObjectData {
+  int v_int;
+} lang_object_data_t;
+
+typedef struct LangObject {
+  lang_object_kind_t kind;
+  lang_object_data_t data;
+} lang_object_t;
--- tests/main_test.c
+++ tests/main_test.c
@@ -1,20 +1,33 @@
 #include <stdlib.h>
 
 #include "./munit/munit.h"
+#include "../src/langobject.h"
 
-static MunitResult test(const MunitParameter params[], void* data) {
-  munit_assert(1 == 1);
+static MunitResult test_integer_constant(const MunitParameter params[], void *data) {
+  munit_assert_int(INTEGER, ==, 0);
+
+  return MUNIT_OK;
+}
+
+static MunitResult test_integer_obj(const MunitParameter params[], void *data) {
+  lang_object_t *obj = malloc(sizeof(lang_object_t));
+  obj->kind = INTEGER;
+  obj->data.v_int = 0;
+
+  munit_assert_int(obj->kind, ==, INTEGER);
+  munit_assert_int(obj->data.v_int, ==, 0);
 
   return MUNIT_OK;
 }
 
 static MunitTest test_suite_tests[] = {
-    {"test", test, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+    {"/integer_constant", test_integer_constant, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+    {"/integer_obj", test_integer_obj, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
     {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
 };
 
 static const MunitSuite test_suite = {
-    "", test_suite_tests, NULL, 1, MUNIT_SUITE_OPTION_NONE,
+    "object_integer_def", test_suite_tests, NULL, 1, MUNIT_SUITE_OPTION_NONE,
 };
 
 int main(int argc, char* argv[MUNIT_ARRAY_PARAM(argc + 1)]) {
