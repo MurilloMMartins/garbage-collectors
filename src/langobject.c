6469bc776bcb6867880a778b88b69ae9fc2bb4c2
MurilloMMartins <murillomartins19@gmail.com>
2025-06-25 16:31:41 -0300

    Implementing string objects

--- src/langobject.c
+++ src/langobject.c
@@ -1,9 +1,22 @@
 #include "langobject.h"
 
 #include <stdlib.h>
+#include <string.h>
 
 lang_object_t *new_lang_string(char *value) {
-  return NULL;
+  lang_object_t *obj = (lang_object_t *)malloc(sizeof(lang_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+
+  obj->kind = STRING;
+  obj->data.v_string = (char *)malloc(sizeof(char) * (strlen(value) + 1));
+  if (obj->data.v_string == NULL) {
+    return NULL;
+  }
+  strcpy(obj->data.v_string, value);
+
+  return obj;
 }
 
 lang_object_t *new_lang_float(float value) {
